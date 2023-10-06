ec82c4d5bdc5fe9479098947632bf56b3fa66f3e
thomas.fischer <thomas.fischer@sully-group.com>
2023-10-06 12:13:24 +0200

    tp2

--- src/entities/Personne.java
+++ src/entities/Personne.java
@@ -1,5 +1,15 @@
 package entities;
 
-public class Personne {
+public class Personne{
+	public String nom;
+	public String prenom;
+	public AdressePostale adresse;
+	
+	 public Personne(String name, String surname, AdressePostale adr) {
+		nom = name;
+		prenom = surname;
+		adresse = adr;
+	}
+
 
 }
--- src/entities/TestPersonne.java
+++ src/entities/TestPersonne.java
@@ -10,8 +10,15 @@ public class TestPersonne {
 		int codePostallyn = 34000;
 		String citylyn = "Lyon";
 		AdressePostale lyon = new AdressePostale(numRuelyn, nomRuelyn, codePostallyn, citylyn);
+		String name1 = "Isidor";
+		String surname1 = "Jules";
+		int numRueOrl = 17;
+		String nomRueOrl = "Saint Louis";
+		int codePostalOrl = 45240;
+		String cityOrl = "Orléans";
+		AdressePostale orleans = new AdressePostale(numRueOrl, nomRueOrl, codePostalOrl, cityOrl);
 		Personne mrX = new Personne(name, surname, lyon);
-		Personne mrY = new Personne(name, surname, lyon);
+		Personne mrY = new Personne(name1, surname1, orleans);
 	}
 
 }
