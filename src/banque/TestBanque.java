f75075ce3ef4e91e9ca3d2382b7ad179994c8d37
thomas.fischer <thomas.fischer@sully-group.com>
2023-10-09 13:53:38 +0200

    ajout comptes.length dans la boucle foreach

--- src/banque/TestBanque.java
+++ src/banque/TestBanque.java
@@ -19,7 +19,7 @@ public class TestBanque {
 		
 		comptes[1] = monCompteTaux;
 		
-		for(int i = 0; i < 2; i++) {
+		for(int i = 0; i < comptes.length; i++) {
 			System.out.println(comptes[i]);
 		}
 	}
