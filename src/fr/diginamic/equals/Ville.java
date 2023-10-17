20cb56b46a3b461ebcb943a61e6ad38754f73fd4
thomas.fischer <thomas.fischer@sully-group.com>
2023-10-17 16:40:29 +0200

    javadoc sur classe ville du tp equals

--- src/fr/diginamic/equals/Ville.java
+++ src/fr/diginamic/equals/Ville.java
@@ -1,10 +1,19 @@
 package fr.diginamic.equals;
 
+/**
+ * 
+ */
+/**
+ * 
+ */
 public class Ville {
+	/** nom  */
 	private String nom;
+	/** population  */
 	private int population;
 	
-	/**
+
+	/** Constructeur
 	 * @param nom
 	 * @param population
 	 */
@@ -28,29 +37,28 @@ public class Ville {
 		return "Ville [nom=" + nom + ", population=" + population + "]";
 	}
 
-
-	/**
+	/** Getter
 	 * @return the nom
 	 */
 	public String getNom() {
 		return nom;
 	}
 
-	/**
+	/** Setter
 	 * @param nom the nom to set
 	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
-	/**
+	/** Getter
 	 * @return the population
 	 */
 	public int getPopulation() {
 		return population;
 	}
 
-	/**
+	/** Setter
 	 * @param population the population to set
 	 */
 	public void setPopulation(int population) {
