92c83f4a75e4f797992e82439a69f9bc7fa3f999
thomas.fischer <thomas.fischer@sully-group.com>
2023-10-18 11:55:09 +0200

    commentaires

--- src/fr/diginamic/testenumeration/TestEnum.java
+++ src/fr/diginamic/testenumeration/TestEnum.java
@@ -7,13 +7,16 @@ public class TestEnum {
 		String nom = "ETE";
 		String libelle = "Hiver";
 		
+		// Parcourt le tableaux de saisons pour afficher toutes les saisons
 		for(Saison s : saisons) {
 			System.out.println(s.getLibelle()); 
 		}
 		
+		// Création d'une instance de Saison qui a pour valeur "ETE" et on y affiche le libelle
 		Saison saisonEte = Saison.valueOf(nom);
 		System.out.println(saisonEte.getLibelle());
 		
+		// Utilisation de la méthode crée dans Saison.java pour retourner une saion en fonction de son libelle
 		Saison saisonHiver = Saison.getSaisonByLibelle(libelle);
 		System.out.println(saisonHiver);
 	}
